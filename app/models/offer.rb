f84af5944b05a505327d7756c673f19c79a87ab8
andgursky <agurskyi@warfare-tec.com>
2018-02-23 13:35:02 +0200

    Offer - model validation fix

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,12 +1,11 @@
 class Offer < ApplicationRecord
-  validates :payout, numericality true
+  validates :payout, presence: true, numericality: true
   validate :payout_grater_zero
   belongs_to :user
 
   private
 
   def payout_grater_zero
-    binding.pry
-    self.payout > 0
+    self.payout && self.payout > 0 ? true : false
   end
 end
