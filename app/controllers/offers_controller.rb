4e3dfdac55bb04e74d4d03e8cfaf4e2fdf8b41a1
andgursky <agurskyi@warfare-tec.com>
2018-02-24 21:01:03 +0200

    OffersController - was implemented completly
    routing was fixed

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,22 +1,36 @@
 class OffersController < ApplicationController
+  respond_to :html, :json
+
   def index
     @offers = Offer.all
   end
 
   def show
-    @my_offers = User.find(params["id"]).offers.as_json
+    @my_offers = User.find(params["id"]).offers
     render :show
   end
 
   def create
-    binding.pry
-    user = User.find params["user_id"]
-    offer = user.offers.create(name: params["name"],
-                               advertiser: params["advertiser"],
-                               payout: params["payout"])
-    @offers << offer
+    if params["offers"]
+      user = User.find params["id"]
+      offer = user.offers.create(name: params["offers"]["name"],
+                                 advertiser: params["offers"]["advertiser"],
+                                 payout: params["offers"]["payout"])
+      redirect_to action: :show
+    else
+      render :create
+    end
   end
 
   def update
+    @offer = Offer.find(params["id"])
+    @offer.update_attributes(offer_params)
+    respond_with @offer
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:name, :advertiser, :payout)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   root to: "offers#index"
-  resources :offers
+  post "/users/:id/offers" => "offers#create", as: "create_offers"
+  get "/users/:id/offers" => "offers#show", as: "show_offers"
   devise_for :users
+  resources :offers, only: %i[index update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
